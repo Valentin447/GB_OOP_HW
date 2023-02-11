1bd212e75b7687afd44f4adff444e95368923460
Valentin447 <valentin@myttk.ru>
2023-02-12 00:16:46 +0700

    Deleted directory intro

--- HomeWork1/src/main/java/ru/gb/lesson1/intro/Cat.java
+++
@@ -1,9 +0,0 @@
-package ru.gb.lesson1.intro;
-
-public class Cat extends Animal {
-
-    public Cat(String name) {
-        super(name);
-    }
-
-}
--- HomeWork1/src/main/java/ru/gb/lesson1/intro/Dog.java
+++
@@ -1,9 +0,0 @@
-package ru.gb.lesson1.intro;
-
-public class Dog extends Animal {
-
-    public Dog(String name) {
-        super(name);
-    }
-
-}
