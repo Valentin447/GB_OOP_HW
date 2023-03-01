a7024d2902bfbc1abbdc31cf6a1a9e49f1c95330
Valentin447 <valentin@myttk.ru>
2023-03-01 17:30:21 +0700

    Init home work 5

---
+++ HomeWork5/src/StringToDirectionExample.java
@@ -0,0 +1,8 @@
+public class StringToDirectionExample {
+
+    public static void main(String[] args) {
+        Direction direction = Direction.ofString("LEFT");
+        System.out.println(direction);
+    }
+
+}
