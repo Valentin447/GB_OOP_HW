c8b1e2c6bbc8bc534fe31d1a9ff400c9460b21c4
Valentin447 <valentin@myttk.ru>
2023-03-01 20:29:08 +0700

    Delete StringToDirectionExample

--- HomeWork5/src/StringToDirectionExample.java
+++
@@ -1,8 +0,0 @@
-public class StringToDirectionExample {
-
-    public static void main(String[] args) {
-        Direction direction = Direction.ofString("LEFT");
-        System.out.println(direction);
-    }
-
-}
