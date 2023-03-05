1ca0ddeb885fa824efbde9438fe0cdd997b59093
Valentin447 <valentin@myttk.ru>
2023-03-05 20:38:29 +0700

    Add solution

--- HomeWork7/src/Employee.java
+++ HomeWork7/src/Employee.java
@@ -1,2 +1,29 @@
+import java.util.Objects;
+
 public class Employee {
+    String firstName;
+    String lastName;
+    String departmentName;
+    Double wage;
+
+    Employee(String firstName, String lastName, String departmentName, Double wage) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.departmentName = departmentName;
+        this.wage = wage;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Employee employee = (Employee) o;
+        return firstName == employee.firstName && lastName == employee.lastName;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
 }
--- HomeWork7/src/HashcodeMain.java
+++ HomeWork7/src/HashcodeMain.java
@@ -1,3 +1,5 @@
+
+
 public class HashcodeMain {
 
     public static void main(String[] args) {
@@ -7,6 +9,15 @@ public class HashcodeMain {
         // Зарплата (double)
         // Реализовать equals и hashCode, который работает только с именем и фамилией
 
+        Employee employee1 = new Employee("Иван", "Иванов", "бухгалтерия", 21345.23);
+        Employee employee2 = new Employee("Иван", "Иванов", "отдел кадров", 41345.23);
+        Employee employee3 = new Employee("Петр", "Петров", "маркетинг", 31345.23);
+
+        System.out.println(employee1.equals(employee2));
+        System.out.println(employee2.equals(employee3));
+        System.out.println(employee1.hashCode());
+        System.out.println(employee2.hashCode());
+        System.out.println(employee3.hashCode());
     }
 
 }
