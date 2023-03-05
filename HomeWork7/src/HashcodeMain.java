7efee709f5c65b59d645e5d18d27b58a9954fd2c
Valentin447 <valentin@myttk.ru>
2023-03-05 20:12:18 +0700

    Init home work 7

---
+++ HomeWork7/src/HashcodeMain.java
@@ -0,0 +1,12 @@
+public class HashcodeMain {
+
+    public static void main(String[] args) {
+        // Создать класс Сотрудник (Employee) с полями:
+        // Имя, Фамилия (String)
+        // Название отдела (String)
+        // Зарплата (double)
+        // Реализовать equals и hashCode, который работает только с именем и фамилией
+
+    }
+
+}
